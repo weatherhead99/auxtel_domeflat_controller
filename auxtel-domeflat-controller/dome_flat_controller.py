cdce8555d8df83454c4d95111fc73223bd646cb7
Dan Weatherill <dan.weatherill@cantab.net>
2022-09-29 20:28:21 -0300

    basic working script

---
+++ auxtel-domeflat-controller/dome_flat_controller.py
@@ -0,0 +1,56 @@
+import argparse
+import sys
+from labjack import ljm # type: ignore
+import socket
+
+
+class DomeFlatController:
+    _PINNAME = "FIO0"
+    def __init__(self, labjackaddr: str):
+        self._labjackaddr = labjackaddr
+        #check if we were passed an IP address
+        try:
+            socket.inet_aton(labjackaddr)
+        except OSError:
+            #this is not an ip address
+            ipaddr = socket.gethostbyname(labjackaddr)
+        else:
+            #this is an IP address
+            ipaddr = labjackaddr
+
+        self._hdl = ljm.open(ljm.constants.dtT7,
+                             ljm.constants.ctETHERNET,
+                             ipaddr)
+
+    @property
+    def domeFlatState(self) -> bool:
+        state = ljm.eReadName(self._hdl, self._PINNAME)
+        return bool(int(state))
+
+    @domeFlatState.setter
+    def domeFlatState(self, state: bool) -> None:
+        ljm.eWriteName(self._hdl, self._PINNAME, float(int(state)))
+
+
+def script():
+    ADDRESS = "auxtel-labjack02.cp.lsst.org"
+    ap = argparse.ArgumentParser("dome_flat_controller",
+                                 description="manually turn on and off the auxtel dome flat")
+
+    ap.add_argument("state", type=int, nargs="?", help="the state to set the flat to. Ignored if --read provided")
+    ap.add_argument("--read", action="store_true", help="if set, read back the value rather than setting it")
+    args = ap.parse_args()
+
+    if args.state is None and not args.read:
+        print("ERROR: must supply either --read or a state to set")
+        sys.exit(-1)
+    elif args.state is None:
+        dfc = DomeFlatController(ADDRESS)
+        print(int(dfc.domeFlatState))
+    else:
+        dfc = DomeFlatController(ADDRESS)
+        dfc.domeFlatState = bool(args.state)
+
+
+if __name__=="__main__":
+    script()
